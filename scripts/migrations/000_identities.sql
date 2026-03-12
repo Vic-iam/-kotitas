0121d869c003b169264bde2d4c24433d995b0479
Juan García <43949658+garcia-s@users.noreply.github.com>
2026-03-12 13:45:14 -0500

    Setting up SSR for vite application

---
+++ scripts/migrations/000_identities.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS identities (
+    id TEXT PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    hash BYTEA,
+    salt BYTEA,
+    cycles INT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
---
+++ server.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express'
+import { createServer as createViteServer } from 'vite'
+import fs from "node:fs"
+
+const app = express()
+const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+})
+
+app.use(vite.middlewares)
+
+app.use('*', async (req: Request, res: Response) => {
+    const url = req.originalUrl
+    let template = fs.readFileSync('index.html', 'utf-8')
+    template = await vite.transformIndexHtml(url, template)
+    const { render } = await vite.ssrLoadModule('/src/entry-server.tsx')
+    const html = render()
+    const result = template.replace('<!--ssr-outlet-->', html)
+    res.status(200).set({ 'Content-Type': 'text/html' }).send(result)
+})
+
+app.listen(5173)
