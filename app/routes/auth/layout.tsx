8c4381f5232a4ec44cdd91ebea658e0b5d81fd97
Juan García <43949658+garcia-s@users.noreply.github.com>
2026-03-20 16:30:31 -0500

    Fixing the dumb fking css colors theme

---
+++ app/routes/auth/layout.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Layout() {
+    <div className="relative min-h-[100svh] flex-1 flex justify-center items-center">
+        <div class="w-full max-w-sm px-4 loading-hide" j
+            {children... }
+    </div>
+    </div >
+}
--- app/routes/auth/register.tsx
+++ app/routes/auth/register.tsx
@@ -8,5 +8,20 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Register() {
-    return <div className="bg-red-500">RegisterPage</div>;
+    return <div className="flex w-full min-h-[100svh] items-center justify-center">
+
+        <div className="bg-surface p-2 rounded">
+            <h1 className="text-xl text-foreground">Login page</h1>
+
+            <form className="flex flex-col">
+                <label>Usuario</label>
+                <input />
+                <label>Contraseña</label>
+                <input />
+            </form>
+
+        </div>
+
+
+    </div>;
 }
